a66d446b8c878c8daa428c3f52f13c47aabd878a
ImaginaryLost <--global>
2018-11-29 20:23:23 -0600

    Updates

---
+++ precache-manifest.3e35e70ac03120233a3ae163d4b5d674.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0461412a40200dccf638",
+    "url": "/bfv/static/css/main.903da732.chunk.css"
+  },
+  {
+    "revision": "0461412a40200dccf638",
+    "url": "/bfv/static/js/main.0461412a.chunk.js"
+  },
+  {
+    "revision": "ee5188b20954baa9eb9f",
+    "url": "/bfv/static/css/1.11a1a016.chunk.css"
+  },
+  {
+    "revision": "ee5188b20954baa9eb9f",
+    "url": "/bfv/static/js/1.ee5188b2.chunk.js"
+  },
+  {
+    "revision": "43a2f772776f0dc55f02",
+    "url": "/bfv/static/js/runtime~main.43a2f772.js"
+  },
+  {
+    "revision": "e58483075281b4407842bd38f531c574",
+    "url": "/bfv/static/media/loading.e5848307.gif"
+  },
+  {
+    "revision": "ccb2a42a5f927e9b04182a1ed7b7df6b",
+    "url": "/bfv/index.html"
+  }
+];
